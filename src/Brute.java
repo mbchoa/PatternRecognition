294d17eacdefc1684fee62e3b6ef6b04a6dec5ce
Michael Choa <mbchoa@gmail.com>
2015-08-18 16:28:17 -0400

    Create array of points

--- src/Brute.java
+++ src/Brute.java
@@ -1,13 +1,17 @@
-import java.io.File;
-
-/**
- * Created by BRYANT on 8/10/2015.
- */
 public class Brute {
 
     public static void main(String[] args) {
-        File inputfile = new File(args[0]);
-        In stream = new In(inputfile);
+        // read in the input
+        String filename = args[0];
+        In in = new In(filename);
+        int N = in.readInt();
+        Point[] pointsArr = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            pointsArr[i] = new Point(x, y);
+        }
+
 
     }
 }
