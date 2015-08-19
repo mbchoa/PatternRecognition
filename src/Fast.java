591a074d5458e65e0f72d09cbd2076a7d72e5588
Michael Choa <mbchoa@gmail.com>
2015-08-18 23:55:57 -0400

    Add Fast.java class

---
+++ src/Fast.java
@@ -0,0 +1,14 @@
+public class Fast {
+    public static void main(String[] args) {
+        // read in the input
+        String filename = args[0];
+        In in = new In(filename);
+        int N = in.readInt();
+        Point[] pointsArr = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            pointsArr[i] = new Point(x, y);
+        }
+    }
+}
