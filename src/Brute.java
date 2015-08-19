3cf8be5f879242d5e79d764420c1db14d8dcd9f5
Michael Choa <mbchoa@gmail.com>
2015-08-18 23:54:36 -0400

    Implement brute force with optimization check for collinearity of first 3 points Fixed slopeTo method return

--- src/Brute.java
+++ src/Brute.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Brute {
 
     public static void main(String[] args) {
@@ -12,6 +14,27 @@ public class Brute {
             pointsArr[i] = new Point(x, y);
         }
 
+        Arrays.sort(pointsArr);
 
+        Stopwatch sw = new Stopwatch();
+        for(int p = 0; p < N; p++) {
+            for(int q = p+1; q < N; q++) {
+                for(int r = q+1; r < N; r++) {
+                    double slopePtoQ = pointsArr[p].slopeTo(pointsArr[q]);
+                    double slopeQtoR = pointsArr[q].slopeTo(pointsArr[r]);
+                    if(slopePtoQ == slopeQtoR) {
+                        for(int s = r+1; s < N; s++) {
+                            double slopeRtoS = pointsArr[r].slopeTo(pointsArr[s]);
+                            if(slopePtoQ == slopeQtoR && slopeQtoR == slopeRtoS)
+                                StdOut.println(pointsArr[p].toString() + " -> " +
+                                        pointsArr[q].toString() + " -> " +
+                                        pointsArr[r].toString() + " -> " +
+                                        pointsArr[s].toString());
+                        }
+                    }
+                }
+            }
+        }
+        StdOut.println(sw.elapsedTime());
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -23,7 +23,7 @@ public class Point implements Comparable<Point> {
             double slopePtoR = slopeTo(r);
             if(slopePtoQ < slopePtoR) return -1;
             if(slopePtoQ == slopePtoR) return 0;
-            if(slopePtoQ > slopePtoR) return 1;
+            return 1;
         }
     }
 
@@ -54,7 +54,7 @@ public class Point implements Comparable<Point> {
         if (this.y == that.y)  return 0.0;
         if (this.x == that.x)  return Double.POSITIVE_INFINITY;
         if (this.x == that.x && this.y == that.y)  return Double.NEGATIVE_INFINITY;
-        return (that.y - this.y) / (that.x - this.x);
+        return (double)(that.y - this.y) / (double)(that.x - this.x);
     }
 
     // is this point lexicographically smaller than that one?
